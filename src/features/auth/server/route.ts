fcb49c7fdf8b99d4a5c4905ea2d6160c3b28e3d6
ritik6559 <ritikjoshi741@gmail.com>
2025-04-14 13:42:19 +0530

    setting up appwrite

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,6 +1,10 @@
 import { Hono } from 'hono';
 import { zValidator } from "@hono/zod-validator";
 import {loginSchema, registerSchema} from "@/features/auth/schemas";
+import {createAdminClient} from "@/lib/appwrite";
+import { ID } from "node-appwrite";
+import {deleteCookie, setCookie} from "hono/cookie";
+import {AUTH_COOKIE} from "@/features/auth/constants";
 
 const app = new Hono()
     .post(
@@ -10,24 +14,51 @@ const app = new Hono()
 
             const { email, password } = c.req.valid("json");
 
-            console.log({
-                email, password
+            const { account }= await createAdminClient();
+            const session = await account.createEmailPasswordSession(email, password);
+            setCookie(c, AUTH_COOKIE, session.secret, {
+                path: "/",
+                httpOnly: true,
+                secure: true,
+                sameSite: "strict",
+                maxAge: 60 * 60 * 24 * 30,
             });
 
-            return c.json({email, password});
+            return c.json({ success: true });
     })
     .post(
         "/register",
         zValidator("json", registerSchema),
         async (c) => {
             const { name, email, password } = c.req.valid("json");
-            console.log({
-                name,
+
+            const { account } = await createAdminClient();
+
+            const user = await account.create(
+                ID.unique(),
                 email,
-                password
+                password,
+                name
+            );
+
+            const session = await account.createEmailPasswordSession(email, password);
+
+            setCookie(c, AUTH_COOKIE, session.secret, {
+                path: "/",
+                httpOnly: true,
+                secure: true,
+                sameSite: "strict",
+                maxAge: 60 * 60 * 24 * 30,
             });
 
-            return c.json({ name, email ,password });
+            return c.json({ success: true });
         })
+    .post(
+        "/logout",
+        async (c) => {
+            deleteCookie(c, AUTH_COOKIE);
+
+            return c.json({ success: true })
+        });
 
 export default app;
